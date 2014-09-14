9f9374aaf1633b928a6b04fca6e19f488bfa9aa6
Nikola Corlija <nikolacorlija@Nikolas-Mac-mini.local>
2014-09-14 14:24:41 +0200

    Initial Commit

---
+++ focus/focus/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  focus
+//
+//  Created by Nikola Corlija on 14/09/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@end
